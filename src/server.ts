65985953c56562c721253eb43be314224c84eeae
M-Sieger <msieger1994@gmail.com>
2021-03-05 15:02:23 +0100

    ✨ Handle GET request

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,41 @@
-DeviceMotionEvent.config();
+import http from "http";
+import dotenv from "dotenv";
+import { connectDB, readPasswordDoc } from "./db";
+
+dotenv.config();
 
 const port = process.env.PORT;
 
-const server = http.createServer((request, response) => {
-  response.statusCode = "200";
-  response.setHeader("Content-Type", "application/json");
-  response.end(JSON.stringify([1, 2, 3]));
-});
+const url = process.env.MONGODB_URL;
 
+connectDB(url, "pw-manager-moritz");
+
+const server = http.createServer(async (request, response) => {
+  if (request.url === "/") {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/html");
+    response.end("<h1>Safe Me!</h1>");
+    return;
+  }
+
+  const parts = request.url.split("/");
+  const passwordName = parts[parts.length - 1];
+
+  if (request.method === "GET") {
+    const passwordDoc = await readPasswordDoc(passwordName);
+    if (!passwordDoc) {
+      response.statusCode = 404;
+      response.end();
+      return;
+    }
+
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "application/json");
+    response.end(JSON.stringify(passwordDoc));
+  }
+
+  response.end();
+});
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
