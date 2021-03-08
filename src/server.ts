cb9682aae3ac433c6438b10d378d7bd650e1eb74
M-Sieger <msieger1994@gmail.com>
2021-03-08 14:09:40 +0100

    add POST request

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import http from "http";
 import dotenv from "dotenv";
-import { connectDB, deletePasswordDoc, readPasswordDoc } from "./db";
+import {
+  connectDB,
+  deletePasswordDoc,
+  readPasswordDoc,
+  PasswordDoc,
+  createPasswordDoc,
+} from "./db";
 
 dotenv.config();
 
@@ -17,9 +23,57 @@ const server = http.createServer(async (request, response) => {
     response.end("<h1>Safe Me!</h1>");
     return;
   }
+  const handlePost = async (
+    request: http.IncomingMessage,
+    response: http.ServerResponse
+  ) => {
+    const passwordDoc = await parseJSONBody<PasswordDoc>(request);
+
+    const isSuccesful = await createPasswordDoc(passwordDoc);
+    if (!isSuccesful) {
+      response.statusCode = 400;
+      response.end();
+      return;
+    }
+
+    response.statusCode = 201;
+    response.end();
+  };
+  const parseJSONBody = <T>(request: http.IncomingMessage): Promise<T> => {
+    return new Promise((resolve) => {
+      let json = "";
+      request.on("data", (chunk) => {
+        json += chunk;
+      });
+      request.on("end", () => {
+        const body = JSON.parse(json);
+        resolve(body);
+      });
+    });
+  };
+
+  if (request.method == "POST") {
+    handlePost(request, response);
+  }
 
   const parts = request.url.split("/");
   const passwordName = parts[parts.length - 1];
+
+  const handleGet = async (
+    request: http.IncomingMessage,
+    response: http.ServerResponse,
+    passwordName: string
+  ) => {
+    const passwordDoc = await readPasswordDoc(passwordName);
+    if (!passwordDoc) {
+      response.statusCode = 404;
+      response.end();
+      return;
+    }
+    response.statusCode = 200;
+    response.end();
+    return;
+  };
 
   if (request.method === "GET") {
     const passwordDoc = await readPasswordDoc(passwordName);
